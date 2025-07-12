b9e882ec0f1ee4581a7c36e0de94d7b6e2c95804
Urazmaxambetov Serik <urazmaxambetovserik@gmail.com>
2025-07-12 12:51:25 +0500

    feat: implement tcp transport

--- nest_rpc_client/transports/tcp.py
+++ nest_rpc_client/transports/tcp.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+import uuid
 
 from ..config.tcp import TCPConfig
 from ..transport import Transport
@@ -11,15 +13,53 @@ class TCPTransport(Transport):
 
     def __init__(self, config: TCPConfig):
         self.config = config
+        self.reader = None  # type: ignore
+        self.writer = None  # type: ignore
 
     async def connect(self):
-        raise NotImplementedError
+        self.reader, self.writer = await asyncio.open_connection(
+            self.config.host,
+            self.config.port,
+        )
 
     async def close(self):
-        raise NotImplementedError
+        if self.writer:
+            self.writer.close()
+            await self.writer.wait_closed()
 
     async def send(self, pattern: str, data: dict) -> dict:
-        raise NotImplementedError
+        correlation_id = str(uuid.uuid4())
+        body = json.dumps(
+            {"id": correlation_id, "pattern": pattern, "data": data},
+        )
+        body_with_length = f"{len(body)}#{body}"
+
+        self.writer.write(body_with_length.encode("utf-8"))
+        await self.writer.drain()
+
+        buffer = ""
+        while True:
+            chunk = (await self.reader.read(4096)).decode()
+            if not chunk:
+                break
+
+            buffer += chunk
+            if "#" in buffer:
+                break
+
+        length_str, rest = buffer.split("#", 1)
+        expected_length = int(length_str)
+
+        while len(rest) < expected_length:
+            rest += (await self.reader.read(4096)).decode()
+
+        response_json = json.loads(rest[:expected_length])
+        return response_json["response"]
 
     async def emit(self, pattern: str, data: dict) -> None:
-        raise NotImplementedError
+        payload = {"pattern": pattern, "data": data}
+        body = json.dumps(payload)
+        body_with_length = f"{len(body)}#{body}"
+
+        self.writer.write(body_with_length.encode("utf-8"))
+        await self.writer.drain()
